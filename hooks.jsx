b98840273d89271caefff7307428d32c52ce7aa5
Walter Melo <41174751+walmello@users.noreply.github.com>
2023-04-25 23:50:58 -0300

    Create hooks.jsx

---
+++ hooks.jsx
@@ -0,0 +1,17 @@
+function useChildren(props, index = 0) {
+    const children = !Array.isArray(props.children)
+        ? props.children
+        : props.children[index]
+    return children
+}
+
+function useComponent(props, index = 0) {
+    const children = useChildren(props, index)
+    const element = children.props.children
+    return (subs) => {
+        console.log(subs)
+        element.props = subs
+        //element.props = { ...element.props, subs }
+        return cloneElement(children, subs)
+    }
+}
